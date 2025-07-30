c2c37c00b9bb39a352a5d64ceac244d588827b66
Ankit Verma <akv956549@gmail.com>
2025-07-30 20:56:59 +0530

    nltk updated

--- app.py
+++ app.py
@@ -5,6 +5,28 @@ import streamlit as st
 import pickle
 from nltk.stem.porter import PorterStemmer
 
+import nltk
+import ssl
+
+# Handle SSL certificate issues
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
+
+# Download required NLTK data
+try:
+    nltk.data.find('tokenizers/punkt_tab')
+except LookupError:
+    nltk.download('punkt_tab')
+
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
 # Download required NLTK data (add this for first-time setup)
 try:
     nltk.data.find('tokenizers/punkt')
